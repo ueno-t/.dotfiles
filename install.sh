a2ac578aaab6c04e7f99f416e19fe4acbf5de7a2
ueno-t <uenotoshiyuki@gmail.com>
2022-05-24 10:20:21 +0900

    remove uninstall old docker process from install.sh

--- install.sh
+++ install.sh
@@ -62,10 +62,8 @@ initialize() {
 }
 
 docker() {
-  # remove old package
-  sudo apt update && sudo apt dist-upgrade
-  sudo apt remove docker docker-engine docker.io containerd runc
   # add docker apt repository
+  sudo apt update && sudo apt dist-upgrade
   sudo apt install -y ca-certificates curl gnupg lsb-release
   curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
   echo   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
